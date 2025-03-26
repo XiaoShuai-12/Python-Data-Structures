8f76ae443864d586bcca2c76129c184e80d748ed
XiaoShuai <2846570993@qq.com>
2025-03-26 15:25:33 +0800

    完成python链表实现（本地提交）

--- linked_list/python_version/linked_list.py
+++ linked_list/python_version/linked_list.py
@@ -1,1 +1,37 @@
-#链表python实现
+## 链表python实现
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def append(self, data):
+        """在链表末尾添加一个新节点"""
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+            return
+        last_node = self.head
+        while last_node.next:
+            last_node = last_node.next
+        last_node.next = new_node
+
+    def print_list(self):
+        """打印链表中的所有节点"""
+        current_node = self.head
+        while current_node:
+            print(current_node.data, end=" -> ")
+            current_node = current_node.next
+        print("None")
+
+# 示例用法
+linked_list = LinkedList()
+linked_list.append(1)
+linked_list.append(2)
+linked_list.append(3)
+linked_list.print_list()
+
